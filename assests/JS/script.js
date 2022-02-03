4e33114bbd5d8c0bb2a8815d649cd9b7a559192a
mediazjr <mediazjr@gmail.com>
2022-02-02 18:23:24 -0800

    added variables linked to ids in html

---
+++ assests/JS/script.js
@@ -0,0 +1,10 @@
+// variables
+const apiKey = '44dc8a0e81bdaed2225d3b963b07a67b';
+const cities = [];
+const cityInputEl = document.querySelector("#city")
+const citySubmitEl = document.querySelector("#city-submit");
+const weatherContainerEl = document.querySelector("#current-weather-container");
+const forecastContainerEl = document.querySelector("#fiveday-container");
+const pastSearchButtonEl = document.querySelector("#past-search-buttons");
+const citySearchInputEl = document.querySelector("#searched-city");
+const forecastTitle = document.querySelector("#forecast");
